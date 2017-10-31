3d1c44efcaadb24414c9c7d25198aacb3ac13d9e
OrangeGuo <1772086465@qq.com>
2017-10-31 22:00:56 +0800

    add BPnetwork

--- BPnetwork.py
+++ BPnetwork.py
@@ -1,24 +1,45 @@
+#coding:utf-8
+import  string
 import numpy as np
 import sklearn.datasets
-import matplotlib.pyplot as plt
-
-input_layers_num = 2
-hidden_layers_num = 8
-output_layers_num = 2
-sample_num = 200
+import math
 epsilion = 0.01
 reg_lambda = 0.01
-# X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-# y = np.array([[0, 1, 1, 0]]).T
+np.random.seed(0)
 
-
+input_layers = 1
+hidden_layers = 5
+output_layers = 1
+X = []
+y = []
+loop = 0
+temp = 0.0
+index = 0.0
+for line in open('data/CSR3.DAT'):
+    s = line.strip().split()
+    if s :
+        loop+=1
+        temp += string.atof(s[0])
+        index += string.atof(s[1])
+        X.append(np.array([temp]))
+        y.append(index)
+X = np.array(X)
+y = np.array(y)
+sample_num = loop
+X_max = X[loop-1][0]
+X_min = X[0][0]
+y_max = y[loop-1]
+y_min = y[0]
+X = (X-X_min)/(X_max-X_min)
+y = (y-y_min)/(y_max-y_min)
+# y =np.array([y]).T
 def net_init():
-    sample, result = sklearn.datasets.make_moons(sample_num, noise=0.20)
-    W1 = 2 * np.random.random(( hidden_layers_num,input_layers_num)) - 1
-    W2 = 2 * np.random.random((output_layers_num,hidden_layers_num)) - 1
-    b1 = np.zeros((hidden_layers_num,1))
-    b2 = np.zeros((output_layers_num,1))
-    net = {'sample':sample,'result':result,'W1':W1,'W2':W2,'b1':b1,'b2':b2}
+    # sample, result = sklearn.datasets.make_moons(sample_num, noise=0.20)
+    W1 = 2 * np.random.random(( hidden_layers,input_layers)) - 1
+    W2 = 2 * np.random.random((output_layers,hidden_layers)) - 1
+    b1 = np.zeros((hidden_layers,1))
+    b2 = np.zeros((output_layers,1))
+    net = {'sample':X,'result':y,'W1':W1,'W2':W2,'b1':b1,'b2':b2}
     return net
 
 def train(net,loops=20000):
@@ -29,10 +50,9 @@ def train(net,loops=20000):
         a2 = np.tanh(z2)
         z3  = W2.dot(a2)+b2
         exp_scores = np.exp(z3)
-        probs = exp_scores/np.sum(exp_scores,axis=0,keepdims=True)
 
-        delta3 = probs
-        delta3[result,range(sample_num)]-=1
+        delta3 = exp_scores - y
+        # delta3[result,range(sample_num)]-=1
         # print delta3[result,range(sample_num)]-2
         dW2 = delta3.dot(a2.T)
         db2 = np.sum(delta3,axis=1,keepdims=True)
@@ -48,44 +68,24 @@ def train(net,loops=20000):
         b1 += -epsilion*db1
         b2 += -epsilion*db2
         net = {'sample': sample, 'result': result, 'W1': W1, 'W2': W2, 'b1': b1, 'b2': b2}
-        if i % 100 == 0:
-            print("loss after iteration %i: %f" % (i, loss_function(net)))
-            axis.append(loss_function(net))
-    with open('data/BPnetwork.txt','w') as file:
-        s= '\t\r'.join(str(i) for i in axis)
-        file.write(s)
-        file.close()
+
     return net
-
-def loss_function(net):
-    sample,result,W1,W2,b1,b2 =net['sample'],net['result'], net['W1'],net['W2'],net['b1'],net['b2']
-    z2 = W1.dot(sample.T)+b1
-    a2 = np.tanh(z2)
-    z3 = W2.dot(a2)+b2
-    exp_scores = np.exp(z3)
-    probs = exp_scores/np.sum(exp_scores,axis=0,keepdims=True)
-    corect_logprobs = -np.log(probs[result, range(sample_num)])
-    data_loss = np.sum(corect_logprobs)
-    data_loss += reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
-    return 1.0 / sample_num * data_loss
-
 def predict(net):
     sample,result,W1,W2,b1,b2 =net['sample'],net['result'],net['W1'],net['W2'],net['b1'],net['b2']
     z2 = W1.dot(sample.T)+b1
     a2 = np.tanh(z2)
     z3 = W2.dot(a2)+b2
     exp_scores = np.exp(z3)
-    probs =exp_scores/ np.sum(exp_scores,axis=0,keepdims=True)
-    s = np.argmax(probs,axis=0)
-    for j in range(sample_num):
-        if(s[j] != result[j]):
-            s[j] = -1
-    return np.argmax(probs, axis=0)
-
+    X=X*(X_max-X_min)+X_min
+    exp_scores = exp_scores*(y_max-y_min)+y_min
+    with open('Data/BPnetwork.txt', 'w') as file:
+        s = '\t\r'.join(str(i[0]) for i in (np.array(X)))
+        file.write(s)
+        file.write('\r')
+        s = '\t\r'.join(str(i[0]) for i in (np.array(exp_scores)))
+        file.write(s)
+        file.close()
+    # print exp_scores
 net = net_init()
 output = train(net)
-predict(net)
-# plt.figure('predict')
-# plt.bar(range(len(output)),output)
-# plt.title('outputs')
-# plt.show()
+predict(output)
